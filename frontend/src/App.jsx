9106d1f85f5b48c8ab84fd9ff890fb76b2cce7e4
predondo29 <101838472+PinGuUwU@users.noreply.github.com>
2026-03-17 15:16:40 -0300

    Se agregó la página de inicio funcional

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,7 +11,6 @@ function App() {
 
     return (
         <BrowserRouter>
-            <ToastProvider />
             <div className='flex'>
                 <NavBar setPlan={setPlan} plan={plan} />
                 <main className='w-full'>
